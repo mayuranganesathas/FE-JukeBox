80115a38af94592188bfbb53d055e370f27759c3
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2022-03-16 08:37:44 -0400

    mg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
 import "tailwindcss/tailwind.css";
 import Block from "../components/ui/Block";
 import Footer from "../components/footer/Footer";
-import ethers from "ethers";
+import { ethers } from "ethers";
+import abi from "../utils/JukeBoxData.json";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 declare global {
   interface Window {
@@ -15,8 +13,11 @@ declare global {
 }
 
 export interface HomeTypes {}
-
 export const Home = ({}: HomeTypes) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [submitLink, setSubmitLink] = useState("");
+  const contractAddress = "0x8DeeC618262Fa586293E20B4400505b2a6598fF3";
+  const contractABI = abi.abi;
   const playSong = () => {
     return null;
   };
@@ -35,7 +36,70 @@ export const Home = ({}: HomeTypes) => {
       } else {
         console.log("We have the ethereum object", ethereum);
       }
-    } finally {
+
+      const accounts: any = await ethereum.request({ method: "eth_accounts" });
+      if (accounts == null || accounts == undefined) {
+        console.log("No authorized account found");
+      } else if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts: any = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const submitLinkForm = () => {
+    submitSong();
+    setSubmitLink("");
+  };
+
+  const submitSong = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum as any);
+        const signer = provider.getSigner();
+        const jukeBoxContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let linkTxn = await jukeBoxContract.jukeBoxPlay(submitLink);
+        console.log("Mining...", linkTxn.hash);
+
+        await linkTxn.wait();
+        console.log("Mined!", linkTxn.hash);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -44,8 +108,29 @@ export const Home = ({}: HomeTypes) => {
       <div className="text-center ">
         <Title />
       </div>
+      {!currentAccount && (
+        <button
+          className="bg-yellow-300 p-2 rounded-xl text-center flex justify-center"
+          onClick={connectWallet}
+        >
+          Connect Wallet
+        </button>
+      )}
       <div className="text-center justify-center">
         <Block playButton={playSong} />{" "}
+        <input
+          className="bg-gray-200"
+          placeholder="Input Spotify Link"
+          type="text"
+          onChange={(e) => setSubmitLink(e.target.value)}
+        />
+        <button
+          className="bg-yellow-300 p-2 rounded-xl text-center flex justify-center"
+          onClick={submitLinkForm}
+        >
+          {" "}
+          Submit{" "}
+        </button>
       </div>
       <div className="bg-gray-500 w-full h-full">
         <Footer />
