8a241379a2781ecdfd7f13ac8b5f63bbd3ca4022
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2022-03-16 08:41:50 -0400

    mg

--- components/Title.tsx
+++ components/Title.tsx
@@ -6,9 +6,9 @@ export const Title = ({}: TitleProps) => {
       <div className="font-bold text-4xl py-4">📻Juke Box📻</div>
       <div className="">
         An onchain record of social music interest. Biased with who uses it.
-        Select the contracts current song, or add one you'd like using spotify{" "}
+        Select the contracts current song, or add one you&apos;d like using
+        spotify{" "}
         <span className="text-[#1DB954] text-xs">
-          {" "}
           https://open.spotify.com/track/15OlC497ScJt9N2BS8lOev?si=f99d587b90644e30
         </span>
       </div>
